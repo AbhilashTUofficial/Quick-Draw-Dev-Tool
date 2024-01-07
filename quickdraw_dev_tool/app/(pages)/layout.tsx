b400ebabefe80325a128347c9b21f0ff77a296b3
abhilashtuofficial <abhilashstorm@gmail.com>
2024-01-08 00:22:27 +0530

    another vercel error

--- quickdraw_dev_tool/app/(pages)/layout.tsx
+++ quickdraw_dev_tool/app/(pages)/layout.tsx
@@ -6,8 +6,7 @@ export const metadata = {
 }
 
 export default function RootLayout({
-  children,
-}: {
+  children}: {
   children: React.ReactNode
 }) {
   return (
--- quickdraw_dev_tool/app/Components/NaviagationBar/NavigationBar.tsx
+++ quickdraw_dev_tool/app/Components/NaviagationBar/NavigationBar.tsx
@@ -1,11 +1,13 @@
 'use client'
 import React from 'react'
 
-const NavigationBar = ({children}) => {
+const NavigationBar = ({ children }: {
+    children: React.ReactNode
+}) => {
     console.log("NavigationBar: loaded");
     return (
-        <div  className=" lg:w-[700px] navbar flex justify-between shadow-lg  shadow-indigo-600/20 mb-4 rounded-lg group/title ">
-        {children}
+        <div className=" lg:w-[700px] navbar flex justify-between shadow-lg  shadow-indigo-600/20 mb-4 rounded-lg group/title ">
+            {children}
         </div>
     )
 }
